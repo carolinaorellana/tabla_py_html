9ebf5d8d04b7b43ed88c74d49434015b01e8bce7
CarolinaOrellana <caryorellanam@gmail.com>
2022-09-27 00:48:06 -0300

    tarea lista

---
+++ aplicacion/controllers/controlador.py
@@ -0,0 +1,12 @@
+from aplicacion import app
+from flask import render_template
+
+@app.route('/')
+def usuarios_info():
+    users = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+    return render_template('index.html', users=users)
